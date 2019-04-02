0a0de5c788afa5e925aa1f2d8b948d7d81429652
kq <oxyzhg@gmail.com>
2019-04-02 23:54:05 +0800

    feat:新增布局

---
+++ src/layouts/default/BaseLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-container class="page">
+    <el-header class="page__hd">
+      <slot name="header"></slot>
+    </el-header>
+    <el-main class="page__bd">
+      <slot name="main"></slot>
+    </el-main>
+    <el-footer class="page__ft">
+      <div>© {{ dateRange }} 山东理工大学学生工作处</div>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+import './style.scss';
+export default {
+  name: 'BaseLayout',
+  computed: {
+    dateRange() {
+      const start = '2016';
+      const current = new Date().getFullYear();
+      return `${start}-${current}`;
+    }
+  }
+};
+</script>
