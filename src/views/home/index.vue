8911a86f2bc1bcb02fbcb92cc3afde7ea65613a4
yhlchao <yhlchao@gmail.com>
2019-09-06 23:00:16 +0800

    feat: 添加通告展示页面

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,55 @@
 <template>
   <div>
-    <h1>This is home page!</h1>
-    <hello-world />
+    <h1>欢迎使用2020年辅导员招聘系统</h1>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="title" label="通知名称"></el-table-column>
+      <el-table-column prop="date" label="发布日期" width="300"></el-table-column>
+      <el-table-column
+        prop="tag"
+        label="查看状态"
+        width="300"
+        :filters="[{ text: '已看', value: '已看' }, { text: '未看', value: '未看' }]"
+        :filter-method="filterTag"
+        filter-placement="bottom-end"
+      >
+        <template slot-scope="scope">
+          <el-tag
+            :type="scope.row.tag === '未看' ? 'primary' : 'success'"
+            close-transition
+          >{{scope.row.tag}}</el-tag>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue';
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      tableData: [
+        {
+          title: '最新通知1',
+          date: '2016-05-02',
+          tag: '已看'
+        },
+        {
+          title: '最新通知2',
+          date: '2016-05-02',
+          tag: '未看'
+        }
+      ]
+    };
+  },
+  methods: {
+    formatter(row, column) {
+      return row.address;
+    },
+    filterTag(value, row) {
+      return row.tag === value;
+    }
+  },
+  components: {}
 };
 </script>
