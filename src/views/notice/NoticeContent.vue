789ad5f8b10accf679e9ebe33afe0db55bf6a913
yhlchao <yhlchao@gmail.com>
2020-02-04 14:47:20 +0800

    feat: 添加通知公告展示功能，优化路由跳转

--- src/views/notice/NoticeContent.vue
+++ src/views/notice/NoticeContent.vue
@@ -1,16 +1,50 @@
 <template>
   <div>
-    content
+    <el-page-header content="公告详情" @back="goBack" />
+    <h1 class="title">{{ postInfo.title }}</h1>
+    <div class="subtitle">发布日期：{{ postInfo.date }}</div>
+    <div class="content">{{ postInfo.content }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'NoticeContent',
+  props: {},
   data() {
-    return {};
+    return {
+      postInfo: {
+        title: '报名须知',
+        date: '2016-05-01',
+        content: '反对户籍卡了的骄傲克劳福德就是卡'
+      }
+    };
+  },
+  methods: {
+    goBack() {
+      // TODO:测试代码
+      console.log('go back');
+      this.$router.back();
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.title {
+  text-align: center;
+  margin-top: 40px;
+  font-size: 20px;
+}
+
+.subtitle {
+  text-align: center;
+  font-size: 12px;
+}
+
+.content {
+  width: 600px;
+  margin: 40px auto 0;
+  font-size: 14px;
+}
+</style>
